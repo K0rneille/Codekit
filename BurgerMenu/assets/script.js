4cdcb03397355e42fb75fd0dbdb0fc8696744ca1
Korneille <nathan.callewaert.22@gmail.com>
2025-11-18 11:15:25 +0100

    BurgerMenu/anim

--- BurgerMenu/assets/script.js
+++ BurgerMenu/assets/script.js
@@ -1,10 +1,10 @@
 "use strict" ;
 
-
+const body = document.querySelector("body");
 const toggle = document.querySelector(".menu__toggle");
 const menu = document.querySelector(".menu");
-const linkList=document.querySelector(".menu_list")
-const link = document.querySelectorAll(".menu__el")
+const linkList=document.querySelector(".menu_list");
+const link = document.querySelectorAll(".menu__el");
 
 if (toggle){
 
@@ -21,7 +21,8 @@ if (toggle){
 
 
 
-function MenuOpen(){
+function MenuOpen(event){
     menu.classList.toggle("menu--open");
+    document.body.classList.toggle("no-scroll");
 }
 

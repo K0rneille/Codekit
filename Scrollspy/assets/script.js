33cdabbe8b78a14e27ae9e9c63754756f4ef6507
Korneille <nathan.callewaert.22@gmail.com>
2025-11-27 10:07:35 +0100

    /maj

--- Scrollspy/assets/script.js
+++ Scrollspy/assets/script.js
@@ -1,1 +1,28 @@
 "use strict" ;
+
+const spyOption ={
+    root:null,
+    rootMargin:"0px",
+    treshold:0.9    
+};
+
+const observer = new IntersectionObserver(ScrollSpy, spyOption);
+function ScrollSpy(entries,_observer){
+    for(let entry of entries){
+        //savoir si la fonction est a 80% dans le viewport//
+        if (entry.isIntersecting){
+            const sectionId = entry.target.getAttribute("id");
+            const active = document.querySelector(".menu__el--active");
+            if(active){
+                active.classList.remove("menu__el--active");
+            }
+            const navActive = document.querySelector('a[href="#'+ sectionId + '"]');
+            navActive.parentElement.classList.add("menu__el--active"); 
+        }
+    }
+}
+
+const sections = document.querySelectorAll(".section");
+for(let section of sections){
+    observer.observe(section);
+}

2e0692a2f00fa659a04c2d7504e77ec6739627d4
Korneille <nathan.callewaert.22@gmail.com>
2026-01-03 13:28:19 +0100

    /newburger

---
+++ BurgerMenu_PIMPE/assets/script.js
@@ -0,0 +1,28 @@
+"use strict" ;
+
+const body = document.querySelector("body");
+const toggle = document.querySelector(".menu__toggle");
+const menu = document.querySelector(".menu");
+const linkList = document.querySelector(".menu_list");
+const link = document.querySelectorAll(".menu__el");
+
+
+if (toggle){
+
+    toggle.addEventListener("click",MenuOpen);
+    
+
+}
+
+    for(let i of link){
+            i.addEventListener("click", MenuOpen); 
+    }
+
+
+
+
+
+function MenuOpen(event){
+    menu.classList.toggle("menu--open");
+    document.body.classList.toggle("no-scroll");
+}
